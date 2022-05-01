c51ece87d2f3b2bc0be799c8e97d4f41a772a027
amoralesc <a.moralesc@javeriana.edu.co>
2022-05-01 14:15:47 -0500

    Create chats and chat fragments

---
+++ app/src/main/java/com/abmodel/uwheels/ui/chat/ChatsFragment.kt
@@ -0,0 +1,29 @@
+package com.abmodel.uwheels.ui.chat
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.abmodel.uwheels.databinding.FragmentChatsBinding
+
+class ChatsFragment : Fragment() {
+
+	// Binding objects to access the view elements
+	private var _binding: FragmentChatsBinding? = null
+	private val binding get() = _binding!!
+
+	override fun onCreateView(
+		inflater: LayoutInflater, container: ViewGroup?,
+		savedInstanceState: Bundle?
+	): View {
+		// Inflate the layout and binding for this fragment
+		_binding = FragmentChatsBinding.inflate(inflater, container, false)
+		return binding.root
+	}
+
+	override fun onDestroyView() {
+		super.onDestroyView()
+		_binding = null
+	}
+}
